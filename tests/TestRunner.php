5a621be4c71ad8ae44ec74071833ca2c3fa22ec5
winstars-b.pastukh <b.pastukh@kf.ua>
2019-03-22 21:26:46 +0200

    refactored TestWasRun

--- tests/TestRunner.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$test = new App\WasRun('testMethod');
-echo (int)$test->wasRun;
-$test->run();
-echo (int)$test->wasRun;
-
---
+++ tests/TestWasRun.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\WasRun;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class TestWasRun
+{
+    public function testWasRun()
+    {
+        $test = new WasRun('testMethod');
+        echo (int)$test->wasRun;
+        $test->run();
+        echo (int)$test->wasRun;
+
+    }
+}
+
+(new TestWasRun())->testWasRun();
+
